45b8d51165a5ab2f1381733fcd48851d123aeb8b
manohar malkar <mhmalkar1989@gmail.com>
2018-08-18 15:19:15 +0530

    udated code for individual user messages

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var io = require('socket.io')(http);
 var Mongo = require('mongodb')
 var monk = require('monk');
 var db = monk('localhost:27017/chat_app');
+var bodyParser = require('body-parser')
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 var users = [];
 
@@ -19,10 +25,23 @@ io.use(function(socket, next) {
 
 app.get('/user/:userid', function(req,res){
 	var collection = db.get('users');
-  	collection.findOne({user_id:req.params.userid},function(e,docs){
+	collection.findOne({user_id:req.params.userid},function(e,docs){
     	res.render("index", {
 			userdata: docs,
 		});
+  	});
+	
+});
+
+app.post('/getChatMessages', function(req,res){
+	var collection = db.get('messages');
+  	console.log(req.body.user1);
+  	collection.find(
+  		{$or:[
+  			{user:req.body.user1,to:req.body.user2},
+  			{user:req.body.user2,to:req.body.user1}
+  		]},{ limit : 10 }, function(e,docs){
+    	res.send(docs);
   	});
 	
 });
